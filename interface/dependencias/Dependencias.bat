a19d4019495a99a22c51e8c9de70f4cc95afbbda
Agustin Mercado <agumerca1994@gmail.com>
2025-01-23 21:52:37 -0300

    actualizacion de dependencias

--- interface/dependencias/Dependencias.bat
+++ interface/dependencias/Dependencias.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Instalando dependencias de Python...
 
-pip install requests pandas openpyxl tqdm
+pip install requests pandas openpyxl tqdm tkinter
 
 echo Todas las dependencias se han instalado correctamente.
